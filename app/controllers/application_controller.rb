cc04280459d1a092b8ff8ddbca402218a2c0d7c7
EC2 Default User <ec2-user@ip-172-31-84-200.ec2.internal>
2021-06-12 11:09:34 +0000

    [Add]名前ログイン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,6 @@ class ApplicationController < ActionController::Base
     root_path
   end
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
-  end
+
+
 end
